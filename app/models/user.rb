409a7000dbfaf7a96b7c88b6dbdd54f47527b10a
Maxence Valette <maxence.valette@hotmail.fr>
2019-11-04 19:38:53 +0100

    change user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
 	after_create :welcome_send
 
-	has_many :events, foreign_key: 'admin_id', class_name: "Event"
-	has_many :events
+	
 
 	def welcome_send
   	UserMailer.welcome_email(self).deliver_now
