de78978891cb50e09ef8fefa386c8cc9289ea8d2
Maxence Valette <maxence.valette@hotmail.fr>
2019-11-04 16:44:48 +0100

    remove multiple_of_5?

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,12 @@
 class Event < ApplicationRecord
 	validates :start_date, presence: true
-	validates :duration, :numericality => { :greater_than_or_equal_to => 0}, presence: true, is_multiple_5?
+	validates :duration, :numericality => { :greater_than_or_equal_to => 0}, presence: true
 	validates :title, length: {minimum: 5, maximum: 140}, presence: true
 	validates :description, length: {minimum: 20, maximum: 1000}, presence: true
 	validates :price, numericality: {minimum: 1, maximum: 1000}, presence: true
 	validates :location, presence: true
 end
 
-def is_multiple_5?
+def is_multiple_5
 	self.duration % 5 == 0 
 end
